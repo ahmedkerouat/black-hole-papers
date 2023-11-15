9881da465aa79ec2f90051cd03a04b17bb83929d
ahmedkerouat <105589873+ahmedkerouat@users.noreply.github.com>
2023-11-15 05:44:41 +0100

    get specified article

--- source/main.py
+++ source/main.py
@@ -1,18 +1,29 @@
 import arxiv
-from datetime import datetime
+
+file_path = "source/current_number.txt"
+with open(file_path, "r") as file:
+    current_number = int(file.read().strip())
+next_number = current_number + 1
+with open(file_path, "w") as file:
+    file.write(str(next_number))
 
 query = 'title="black hole"'
-search = arxiv.Search(query=query, sort_by=arxiv.SortCriterion.SubmittedDate, sort_order=arxiv.SortOrder.Ascending)
+search = arxiv.Search(query=query,max_results=next_number, sort_by=arxiv.SortCriterion.SubmittedDate, sort_order=arxiv.SortOrder.Ascending)
 
-# Get the oldest article
-oldest_article = next(search.results())
+all_results = list(search.results())
+print(len(all_results))
+result = all_results[-1]
 
-title = oldest_article.title
-authors = ', '.join(author.name for author in oldest_article.authors)
-abstract = oldest_article.summary
-date = str(oldest_article.published).split(" ")[0]
+title = result.title
+authors = ', '.join(author.name for author in result.authors)
+abstract = result.summary
+date = str(result.published).split(" ")[0]
 
 print(f"Title: {title}")
 print(f"Authors: {authors}")
 print(f"Abstract: {abstract}")
 print(f"Date: {date}")
+
+
+
+
