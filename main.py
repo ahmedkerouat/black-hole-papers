a079e401077d78fe4c864bfef8a6148984e27e60
ahmedkerouat <105589873+ahmedkerouat@users.noreply.github.com>
2024-02-01 20:23:38 +0100

    this should be alright now

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import arxiv
+import urllib.request as ur
+import feedparser
+from datetime import datetime
 
+# Base api query url
+base_url = 'http://export.arxiv.org/api/query?'
+
+# get last paper index
 file_path = "current_number.txt"
 with open(file_path, "r") as file:
     current_number = int(file.read().strip())
@@ -7,25 +13,43 @@ next_number = current_number + 1
 with open(file_path, "w") as file:
     file.write(str(next_number))
 
-query = 'title="black hole"'
-search = arxiv.Search(query=query, sort_by=arxiv.SortCriterion.SubmittedDate, sort_order=arxiv.SortOrder.Ascending)
+# Search parameters
+search_query = 'all:black+hole' # search for black hole
+start = next_number                      # start at the last paper index + 1
+total_results = next_number + 1              # want one result
+results_per_iteration = 1       # one result at a time
 
-all_results = list(search.results())
-result = all_results[next_number]
+for i in range(start,total_results,results_per_iteration):
+    
+    
+    query = 'search_query=%s&start=%i&max_results=%i&sortBy=submittedDate&sortOrder=ascending' % (search_query,
+                                                         i,
+                                                        results_per_iteration)
 
-title = f"[{result.title}]({result.entry_id})"
-authors = ', '.join(author.name for author in result.authors)
-abstract = result.summary.replace("\n", " ") #remove line breaks
-date = str(result.published).split(" ")[0]
+    # perform a GET request using the base_url and query
+    response = ur.urlopen(base_url+query).read()
 
-readme_path = "README.md"
-new_paper_info = f"| {title} | {authors} | {date} | {abstract} |\n"
+    # parse the response using feedparser
+    feed = feedparser.parse(response)
 
-with open(readme_path, "r") as readme_file:
-    readme_content = readme_file.readlines()
-papers_index = readme_content.index("## License\n")
+    # Run through each entry, and print out information
+    for entry in feed.entries:
 
-readme_content.insert(papers_index -1, new_paper_info)
-with open(readme_path, "w") as readme_file:
-    readme_file.writelines(readme_content)
+        title = f"[{entry.title}]({entry.id})"
+        authors = ', '.join(author.name for author in entry.authors)
+        abstract = entry.summary.replace("\n", " ") #remove line breaks
+        date = str(entry.published).split(" ")[0]
+        datetime_object = datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
+        date = datetime_object.strftime('%Y-%m-%d')
+        
+        # write into the readme
+        readme_path = "README.md"
+        new_paper_info = f"| {title} | {authors} | {date} | {abstract} |\n"
 
+        with open(readme_path, "r") as readme_file:
+            readme_content = readme_file.readlines()
+        papers_index = readme_content.index("## License\n")
+
+        readme_content.insert(papers_index -1, new_paper_info)
+        with open(readme_path, "w") as readme_file:
+            readme_file.writelines(readme_content)
